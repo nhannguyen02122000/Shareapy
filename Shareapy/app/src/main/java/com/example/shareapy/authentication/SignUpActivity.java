d3f21ec2103559576b8faee620c5bfe82c06f151
Nguyễn Đăng Huy <huy.nguyen.gl99@hcmut.edu.vn>
2020-03-11 00:08:50 +0700

    Init UI

---
+++ Shareapy/app/src/main/java/com/example/shareapy/authentication/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.example.shareapy.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.shareapy.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
